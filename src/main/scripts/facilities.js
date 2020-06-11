1ab76a3de541bfc3c13630f3e51ab5f09dca58ea
Mike Radford <mike.radford@disney.com>
2020-06-10 22:02:17 -0700

    Add search box where the PDF and "Get the Code" link was

---
+++ src/main/scripts/facilities.js
@@ -0,0 +1,26 @@
+const input = document.createElement('input');
+input.type = 'search';
+input.id = 'search';
+input.classList.add("d-print-none");
+input.setAttribute('placeholder', 'Search Facilities');
+const [ navElem ] = document.getElementsByTagName('nav')
+navElem.appendChild(input);
+
+const sortColumns = Array.from(document.querySelectorAll('thead th'))
+const codeColumns = Array.from(document.querySelectorAll('tbody .code'))
+const descColumns = Array.from(document.querySelectorAll('tbody .description'))
+const rows = descColumns.map(({ parentNode }) => parentNode)
+const descriptions = descColumns.map(({ textContent }) => textContent.toLowerCase())
+const codes        = codeColumns.map(({ textContent }) => textContent.toLowerCase())
+
+function filter(value) {
+  const valueLc = value.toLowerCase()
+  descriptions.forEach((desc, i) => {
+    const method = (!value || desc.includes(valueLc) || codes[i].includes(valueLc)) ? "remove" : "add"
+    rows[i].classList[method]('d-none');
+  });
+}
+
+document.getElementById('search').addEventListener('input', ({ target }) => { filter(target.value) });
+
+filter('');
