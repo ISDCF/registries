fc9ceccc5bb0d4c59ca984cbbf429336af521272
Mike Radford <Mike.Radford@disney.com>
2020-07-23 18:04:48 -0700

    Check for non-existent obsoletedBy values

--- src/main/scripts/studios.validate.js
+++ src/main/scripts/studios.validate.js
@@ -5,5 +5,11 @@ module.exports = (registry, name) => {
       throw name + " registry key " + registry[i-1].code + " is " +
         ((registry[i-1].code === registry[i].code) ? "duplicated" : "not sorted");
     }
+
+    /* ensure all obsoletedBy codes are found */
+    (registry[i].obsoletedBy||[]).forEach(obs => {
+      if (!registry.find(r => r.code === obs))
+        throw `${name}: ${registry[i].description} is obsoletedBy '${obs}' which is an invalid code`
+    })
   }
 }
