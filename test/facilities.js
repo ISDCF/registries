b0a2b42b7eb7a12bfb6154fe9386dfd8b98f3e62
Steve LLamb <steve.llamb@bydeluxe.com>
2020-07-27 14:19:02 -0700

    Updated test cases for new fields as requested

--- test/facilities.js
+++ test/facilities.js
@@ -14,9 +14,13 @@ describe("facilities schema", () => {
   it("correct validation", () => {
     assert.doesNotThrow(() => validate([
       { code: "AAA", description: "A A A" },
-      { code: "BBB", description: "B B B", obsolete: true },
-      { code: "CCC", description: "C C C", obsolete: true, comments: [ "foo", "bar" ] },
-      { code: "DDD", description: "D D D", obsolete: true, comments: [ "foo", "bar" ], obsoletedBy: [ 'AAA', 'BBB' ] }
+      { code: "BBB", description: "B B B", url: "http://www.foo.com"},
+      { code: "CCC", description: "C C C", obsolete: true },
+      { code: "DDD", description: "D D D", obsolete: true, comments: [ "foo", "bar" ] },
+      { code: "EEE", description: "E E E", obsolete: true, comments: [ "foo", "bar" ], obsoletedBy: [ 'AAA', 'BBB' ] },
+      { code: "FFF", description: "F F F", obsolete: true, comments: [ "foo", "bar" ], obsoletedBy: [ 'AAA', 'BBB' ], url: "http://www.foo.com"},
+      { code: "GGG", description: "G G G", obsolete: true, comments: [ "foo", "bar" ], obsoletedBy: [ 'AAA', 'BBB' ], contact: { name: "Bob Smith", email: "bob@foo.com", address: "1234 Main St., Anytown, State, Country" } },
+      { code: "HHH", description: "H H H", obsolete: true, comments: [ "foo", "bar" ], obsoletedBy: [ 'AAA', 'BBB' ], url: "http://www.foo.com", contact: { name: "Bob Smith", email: "bob@foo.com", address: "1234 Main St., Anytown, State, Country" } }
     ]))
   })
 
