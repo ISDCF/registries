d508632570276143f36a2c8e2031b6a81f0b1300
Pierre-Anthony Lemieux <pal@palemieux.com>
2020-11-03 12:36:50 -0800

    Fix unit test

--- test/ratings.js
+++ test/ratings.js
@@ -3,16 +3,21 @@ const chai = require('chai')
 const should = chai.should();
 const assert = chai.assert;
 const fs = require('fs');
+const path = require('path');
+
+BUILD_DIR = "build";
+ML_REGISTER_PATH = "src/main/resources/ml-registry/CMR_Ratings_2.4.2.xml";
 
 describe("make ratings registry from MovieLabs registry", () => {
   it("valid", () => {
     assert.doesNotThrow(
       () => {
+        fs.mkdirSync(BUILD_DIR, { recursive: true })
         fs.writeFileSync(
-          "build/ratings.json",
+          path.join(BUILD_DIR, "ratings.json"),
           JSON.stringify(
             make_ratings.convertMLRatingsRegistry(
-              fs.readFileSync("src/main/resources/ml-registry/CMR_Ratings_2.4.2.xml", 'utf8')
+              fs.readFileSync(ML_REGISTER_PATH, 'utf8')
             ),
             null,
             "  "
